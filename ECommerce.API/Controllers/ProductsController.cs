1e608b3f831f4f80d5ff5c0b834ca2c0b0a8f910
morcodou <morcodou@gmail.com>
2020-07-04 20:22:14 -0400

    adding web api service fabric

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ApiProduct>> GetAsync()
+        {
+            return new[] { new ApiProduct { Id = Guid.NewGuid(), Description ="fakeApiprod" } };
+        }
+
+        [HttpPost]
+        public async Task PostAsync([FromBody] ApiProduct product)
+        {
+        }
+    }
+}
---
+++ ECommerce.API/Model/ApiProduct.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.API.Model
+{
+    public class ApiProduct
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonProperty("name")]
+        public string    Name { get; set; }
+        [JsonProperty("description")]
+        public string    Description { get; set; }
+        [JsonProperty("price")]
+        public double Price{ get; set; }
+        [JsonProperty("isAvailability")]
+        public bool IsAvailability { get; set; }
+    }
+}
